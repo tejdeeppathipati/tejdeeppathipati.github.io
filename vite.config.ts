c78659e8b1b093e5d4785c7f2f70cdcfe088de71
tejdeeppathipati <tejdeepp0909@gmail.com>
2025-08-26 15:11:30 -0400

    update-package

--- vite.config.ts
+++ vite.config.ts
@@ -3,20 +3,15 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
+  base: "/",
+  build: {
+    outDir: "docs",          
+    emptyOutDir: true,
   },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+  server: { host: "::", port: 8080 },
+  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+    alias: { "@": path.resolve(__dirname, "./src") },
   },
 }));
